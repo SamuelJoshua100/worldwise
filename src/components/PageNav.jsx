c63db447eabba6ddae0365f852d94778e43dce95
Samuel Joshua <144678788+SamuelJoshua100@users.noreply.github.com>
2025-02-11 15:16:44 +0100

    Add files via upload

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -5,7 +5,13 @@ function PageNav() {
     <nav>
       <ul>
         <li>
-          <Link></Link>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/pricing">Pricing</Link>
+        </li>
+        <li>
+          <Link to="/product">Product</Link>
         </li>
       </ul>
     </nav>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Homepage() {
   return (
     <div>
       <h1>WoldWise</h1>
+
+      <Link to="/pricing">Pricing</Link>
     </div>
   );
 }
